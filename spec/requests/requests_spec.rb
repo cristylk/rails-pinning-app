212f1e3e8e1c8498d6756257379ff2e4c8f385f0
cristylk <cristy.koebler@gmail.com>
2015-08-19 13:41:47 -0400

    updated routes

--- spec/requests/requests_spec.rb
+++ spec/requests/requests_spec.rb
@@ -1,5 +1,21 @@
 require "spec_helper"
 
 RSpec.describe "Our Application Routes" do
+
+	describe "GET /pins/name-:slug" do
+
+		it 'renders the pins/show template' do
+			pin = Pin.first
+			get "/pins/name-#{pin.slug}"
+			expect(response).to render_template("pins/show")
+		end
+
+		it 'populates the @pin variable with the apropriate pin' do
+			pin = Pin.first
+			get "/pins/name-#{pin.slug}"
+			expect(assigns[:pin]).to eq(pin)
+		end
+
+	end
   
 end
